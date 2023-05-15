706ec7078bfc22ab8b0f5cab9d1da01f75a8e7bd
varshTPC <89859942+varshTPC@users.noreply.github.com>
2023-05-15 13:47:38 +0530

    Updated settings.py

--- viva-flames/vivapro1/vivapro1/settings.py
+++ viva-flames/vivapro1/vivapro1/settings.py
@@ -135,6 +135,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
